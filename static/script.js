db4566318842835a0276b0ae29aa876045e0304f
aqil iman <aqiliman24@gmail.com>
2024-05-24 23:41:06 +0800

    making static folder

--- static/script.js
+++ static/script.js
@@ -1,67 +1,26 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: 
-            { facingMode:"user",width: { ideal: 640 },height: { ideal: 480 },frameRate : {ideal:30 , max:120}}
-        });
-        const recordedVideo = document.getElementById('recordedVideo');
-        recordedVideo.setAttribute("playsinline",true);
-        recordedVideo.srcObject = stream;
-        recordedVideo.style.display = 'block';
-        const urlParams = new URLSearchParams(window.location.search);
-        const name = urlParams.get("name")
-        const nric = urlParams.get("nric")
 
-        
-        const mediaRecorder = new MediaRecorder(stream);
-        const chunks = [];
-        mediaRecorder.ondataavailable = (event) => chunks.push(event.data);
-        mediaRecorder.onstop = async () => {
-            const blob = new Blob(chunks, { type: 'video/mp4' });
-            const formData = new FormData();
-            formData.append('video', blob, 'video_output.mp4');
-            formData.append("name",name);
-            formData.append("nric",nric)
+function clearInput(){
+    document.getElementById('userInput').value = '';
+    document.getElementById('responseMessage').textContent  = '';
+}
 
-            try {
-                // Sending the video data to the backend
-                const response = await fetch('/upload_video', {
-                    method: 'POST',
-                    body: formData
-                })
-                .then(response => response.json())
-                .then(data => {
-                console.log('Server response:', data);
-                frames = [];
-                console.log(data.name)
-                console.log(data.nric)
-                console.log(data.encodings_match)
-                // Construct the new URL with the query parameter value
-                if (data.encodings_match == true){
-                    const URL_success = "http://localhost:5000/success?name=" + data.name + '&nric='+data.nric + '&match='+data.encodings_match;
-                    // const URL_success = "https://kyc.mkad.my/success?name=" + data.name + '&nric='+data.nric + '&match='+data.encodings_match;
-                    window.location.href = URL_success
-                }
-                else{
-                    const URL_failed = "http://localhost:5000/failed?name=" + data.name + '&nric='+data.nric + '&match='+data.encodings_match;
-                    // const URL_failed = "https://kyc.mkad.my/failed?name=" + data.name + '&nric='+data.nric + '&match='+data.encodings_match; 
-                    window.location.href = URL_failed
-                }
 
-            });
-            } catch (error) {
-                console.error('Error uploading video:', error);
-            }
-        };
+function submitInput() {
+    const inputString = document.getElementById('userInput').value;
 
-        // Auto-stop recording after 1 seconds
-        setTimeout(() => {
-            mediaRecorder.stop();
-            // recordedVideo.style.display = 'none';
-            // stream.getTracks().forEach(track => track.stop());
-        }, 1000);
+    fetch('http://127.0.0.1:8004/chatbot', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: inputString
+    })
+    .then(response => response.text())
+    .then(data => {
+        const responseMessage = document.getElementById('responseMessage');
 
-        mediaRecorder.start();
-    } catch (error) {
-        console.error('Error accessing camera and microphone:', error);
-    }
-});
+        responseMessage.textContent = "This sentence is mostly "+ data;
+        responseMessage.style.color = 'green';
+    })
+    .catch(error => console.error('Error:', error));
+}
